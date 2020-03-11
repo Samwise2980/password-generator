87b9d490731bef6b5dfdedb7885420d19905da44
Samwise <samuelfoster0@gmail.com>
2020-03-10 22:35:20 -0700

    Added the code to generate a random password from decimals to string.

--- script.js
+++ script.js
@@ -15,5 +15,37 @@ function writePassword() {
 // TODO: Write code so the generatePassword returns a string for a password
 // which meets the requirements in the instructions.
 function generatePassword() {
-  return "";
+  // var characterLengthSelect = prompt("How long would you like your password?\n Must be between 8 and 126 long.");
+  // var lowerCaseSelect = prompt("Would you like lowercase characters?\n yes or no");
+  // var upperCaseSelect = prompt("Would you like uppercase characters?\n yes or no");
+  // var numericSelect = prompt("Would like like numeric characters?\n yes or no")
+  // var specialCharacterSelect = prompt("Would you like special characters?\n yes or no");
+
+  // var specialCharacterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", ",", "^", '_', "`", "{", "|", "}", "~"];
+  
+  var randomPass = Math.random().toString(36).substr(2, 8);
+  return randomPass;
 }
+
+// GIVEN I need a new, secure password
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
