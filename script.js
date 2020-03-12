9d1ae0d7bd772bca0dfd089eb68c00e060b87a7a
Samwise <samuelfoster0@gmail.com>
2020-03-11 22:04:30 -0700

    Added password generation for how long you want it and only lower case.

--- script.js
+++ script.js
@@ -25,20 +25,20 @@ function generatePassword() {
     characterLengthSelect = prompt("Please choose a length between 8 and 128 characters");
   }
 
-  // Ask the user if they want lowercase characters
-  var lowerCaseSelect = confirm("Click confirm for lowercase characters");
+  // // Ask the user if they want lowercase characters
+  // var lowerCaseSelect = confirm("Click confirm for lowercase characters");
 
 
-  // Ask the user if they want uppercase characters
-  var upperCaseSelect = confirm("Click confirm for uppercase characters");
+  // // Ask the user if they want uppercase characters
+  // var upperCaseSelect = confirm("Click confirm for uppercase characters");
 
 
-  // Ask if the user wants numeric characters
-  var numericSelect = confirm("Click confirm for numeric characters");
+  // // Ask if the user wants numeric characters
+  // var numericSelect = confirm("Click confirm for numeric characters");
 
   
-  // Ask if the user wants special characters
-  var specialCharacterSelect = confirm("Click confirm for special characters");
+  // // Ask if the user wants special characters
+  // var specialCharacterSelect = confirm("Click confirm for special characters");
 
 
   var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -49,10 +49,11 @@ function generatePassword() {
 
   var specialCharacterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", ",", "^", '_', "`", "{", "|", "}", "~"];
   
+  var randomPass = "";
   // I need to pull each array that the user has chosen
-  function arrayGet() {
-    //switch cases???
-  }
+  // function arrayGet() {
+  //   //switch cases???
+  // }
 
   // I need to create a line that has the length of each array chosen added up to a final number
 
@@ -64,12 +65,13 @@ function generatePassword() {
 
 
   // I need to make a loop as many times as the password length is
-  // for (var i = 0; i < characterLengthSelect; i++) {
+  for (var i = 0; i < characterLengthSelect; i++) {
+    var randomItem = lowerCaseArray[Math.floor(Math.random()*lowerCaseArray.length)];
+    randomPass = randomPass += randomItem;
+    }
+  return randomPass
 
-  // }
-
-  var randomPass = Math.random().toString(36).substr(2, 8);
-  return randomPass;
+//   var randomPass = Math.random().toString(36).substr(2, 8);
 }
 
 // GIVEN I need a new, secure password
