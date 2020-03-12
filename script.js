03b48b29b8a1808db3f526ad18505ed5989c6d39
Samwise <samuelfoster0@gmail.com>
2020-03-11 21:26:53 -0700

    Added my variables for the prompts and confirms

--- script.js
+++ script.js
@@ -15,14 +15,59 @@ function writePassword() {
 // TODO: Write code so the generatePassword returns a string for a password
 // which meets the requirements in the instructions.
 function generatePassword() {
-  // var characterLengthSelect = prompt("How long would you like your password?\n Must be between 8 and 126 long.");
-  // var lowerCaseSelect = prompt("Would you like lowercase characters?\n yes or no");
-  // var upperCaseSelect = prompt("Would you like uppercase characters?\n yes or no");
-  // var numericSelect = prompt("Would like like numeric characters?\n yes or no")
-  // var specialCharacterSelect = prompt("Would you like special characters?\n yes or no");
 
-  // var specialCharacterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", ",", "^", '_', "`", "{", "|", "}", "~"];
+  // Ask the user how long they would want their password length
+  var characterLengthSelect = prompt("How long would you like your password?\n Must be between 8 and 128 long.");
+
+  // If they choose something other than the accepted range
+  while (characterLengthSelect < 8 || characterLengthSelect > 128 || characterLengthSelect == "string") {
+    // Then they need to be prompted to choose an acceptable number
+    characterLengthSelect = prompt("Please choose a length between 8 and 128 characters");
+  }
+
+  // Ask the user if they want lowercase characters
+  var lowerCaseSelect = confirm("Click confirm for lowercase characters");
+
+
+  // Ask the user if they want uppercase characters
+  var upperCaseSelect = confirm("Click confirm for uppercase characters");
+
+
+  // Ask if the user wants numeric characters
+  var numericSelect = confirm("Click confirm for numeric characters");
+
   
+  // Ask if the user wants special characters
+  var specialCharacterSelect = confirm("Click confirm for special characters");
+
+
+  var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  var specialCharacterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", ",", "^", '_', "`", "{", "|", "}", "~"];
+  
+  // I need to pull each array that the user has chosen
+  function arrayGet() {
+    //switch cases???
+  }
+
+  // I need to create a line that has the length of each array chosen added up to a final number
+
+
+  // I need to create a mega array of each of the chosen arrays
+  
+
+  // I need to create a Math.random line that pulls from the mega array using the length from the total array length
+
+
+  // I need to make a loop as many times as the password length is
+  // for (var i = 0; i < characterLengthSelect; i++) {
+
+  // }
+
   var randomPass = Math.random().toString(36).substr(2, 8);
   return randomPass;
 }
